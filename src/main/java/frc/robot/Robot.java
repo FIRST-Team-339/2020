0ec64c5ece6767624936a122faf8d4fd80786c6a
rjbrown124 <rjbrown124@comcast.net>
2020-02-02 16:51:58 -0500

    fixed null pointer

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -157,8 +157,6 @@ public class Robot extends TimedRobot
         // setup
         // ---------------------------------------
         Hardware.launcherMotorGroup.set(0);
-        Hardware.launcherMotorEncoder.setRPM(0, Hardware.launcherMotorGroup);
-
         Hardware.launcherMotorEncoder.speed = 0;
 
         Hardware.leftDriveGroup.set(0);
