ee8b84873870bc0aa877367f8ef8ae4bfa5f52dd
Robert Brown <rjbrown124@comcast.net>
2020-02-05 20:38:49 -0500

    servo

--- src/main/java/frc/Utils/CameraServo.java
+++ src/main/java/frc/Utils/CameraServo.java
@@ -8,35 +8,35 @@ public class CameraServo
 
     public CameraServo()
         {
-
+           
         }
 
     public boolean setCameraAngleUp(JoystickButton button)
     {
-
+        Hardware.rotateServo.setAngle(125);
         //if servo is not at up position move up
         return false;
     }
 
-    public boolean setCameraAngleUp()
+    /*public boolean setCameraAngleUp()
     {
 
         //if servo is not at up position move up
         return false;
-    }
+    }*/
 
     public boolean setCameraAngleDown(JoystickButton button)
     {
         //if servo is not at down position move down
-
+        Hardware.rotateServo.setAngle(55);
         return false;
     }
 
-    public boolean setCameraAngleDown()
+    /*public boolean setCameraAngleDown()
     {
 
         //if servo is not at down position move down
         return false;
-    }
+    }*/
 
     }
